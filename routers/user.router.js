7a29a0f5aae11ef975cf9a0ec8de8ff0048584eb
huongphung256 <huongphung.2506@gmail.com>
2020-01-23 12:59:55 +0700

    Middleware

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,10 +1,22 @@
 var express = require('express');
 
 var controller = require('../controllers/user.controller.js');
+var validate = require('../validate/user.validate');
 
 var router = express.Router();
 
 router.get('/', controller.index);
+
+
+function middleware1(req, res, next) {
+    next();
+};
+
+function middleware2(req, res, next) {
+    res.send('Hello');
+};
+
+router.get('/test', middleware1, middleware2);
 
 router.get('/search', controller.search);
 
@@ -12,6 +24,6 @@ router.get('/create', controller.create);
 
 router.get('/:id', controller.get);
 
-router.post('/create', controller.postCreate);
+router.post('/create', validate.postCreate, controller.postCreate);
 
 module.exports = router;
