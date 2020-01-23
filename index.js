b884f8a845dfe59e93e332e6d6717c4d47f5d476
huongphung256 <huongphung.2506@gmail.com>
2020-01-23 12:19:45 +0700

    Create user validation

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-// const express = require('express');
-// const app = express();
-// const port = 3000;
+var express = require('express');
+var bodyParser = require('body-parser');
+var userRouter = require('./routers/user.router.js');
 
-// app.get('/', function(req, res) {
-//     res.send('Hello World');
-// });
+var port = 3000;
 
-// app.listen(port, function() {
-//     console.log('Server listening on port ' + port);
-// });
-const express = require('express')
-const app = express();
-const port = 3000
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get('/', function(req, res) {
+    res.render('index', {
+        name: 'WORLD'
+    });
+});
+
+app.use(express.static('public'));
+
+app.use('/users', userRouter);
+
+app.listen(port, function() {
+    console.log('Server listening on port ' + port);
+});
